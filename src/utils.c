a355b0ee6bab342f09ce6c12d837acee998c0ed8
iamjunhyeong <dddodo447@gmail.com>
2024-03-23 22:53:57 +0900

    feat utils

---
+++ src/utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 18:17:19 by junhyeop          #+#    #+#             */
+/*   Updated: 2024/03/23 21:49:25 by junhyeop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_token(t_token *token)
+{
+	t_token	*rmv;
+
+	while (token)
+	{
+		rmv = token;
+		token = token->next;
+		free(rmv->cmd);
+		free(rmv);
+	}
+}
+
+void	free_list(t_head *head)
+{
+	t_list	*tmp;
+	t_list	*rmv;
+
+	tmp = head->top;
+	while (tmp)
+	{
+		rmv = tmp; 
+		tmp = tmp->next;
+		free_token(rmv->token);
+		free(rmv);
+	}
+	head->top = NULL;
+}
